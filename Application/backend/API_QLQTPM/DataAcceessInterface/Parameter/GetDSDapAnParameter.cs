40e0784b68f98e3139d3a0639d5f4a2d4d41d289
LuanPham <luanpham.997@gmail.com>
2019-12-16 22:05:20 +0700

    add api getdsdapan

---
+++ Application/backend/API_QLQTPM/DataAcceessInterface/Parameter/GetDSDapAnParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcceessInterface.Parameter
+{
+    public class GetDSDapAnParameter
+    {
+        public int idBaiHoc { get; set; }
+        public int idCapDo { get; set; }
+    }
+}
