26e2e570915444baa3f7c43d90fe5af31dde10b1
DESKTOP-ORDBF2B\Quang Minh <huynhquangminh.cse@gmail.com>
2020-01-19 11:58:02 +0700

    update code admin

---
+++ Application/backend/API_QLQTPM/BusinessLogicInterface/Dtos/GetAllAccountUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogicInterface.Dtos
+{
+   public class GetAllAccountUserDto
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string img { get; set; }
+    }
+}
---
+++ Application/backend/API_QLQTPM/BusinessLogicInterface/Response/GetAllAccountUserResponse.cs
@@ -0,0 +1,11 @@
+﻿using BaseApplication;
+using BusinessLogicInterface.Dtos;
+using System.Collections.Generic;
+
+namespace BusinessLogicInterface.Response
+{
+    public class GetAllAccountUserResponse : BaseResponse
+    {
+        public List<GetAllAccountUserDto> listAccount { get; set; }
+    }
+}
